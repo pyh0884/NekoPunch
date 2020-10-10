14f7267e12fd384295554af55453935e3884f8f7
Daemon Pan <pyh2084@hotmail.com>
2020-10-10 20:29:02 +0800

    Add help functions

--- Assets/Scripts/DragCamera.cs
+++ Assets/Scripts/DragCamera.cs
@@ -1,11 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UI;
 using UnityEngine;
 
-public class DragCamera : Dragable
+public class DragCamera : MonoBehaviour
 {
-    public override void Interact()
+    public Vector3 startPosition;
+    public Vector3 endPosition;
+    private Vector3 targetPosition;
+    public float moveSpeed = 3.0f;
+    private void Start()
     {
-        GetComponentInChildren<MoveCamera>().enabled = true;
+        targetPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        if (Input.mouseScrollDelta.y > 0) 
+        {
+            targetPosition.y += Time.deltaTime * moveSpeed;
+            targetPosition.y = Mathf.Clamp(targetPosition.y, endPosition.y, startPosition.y);
+            transform.position = targetPosition;
+            if (transform.position == endPosition)
+            {
+                FindObjectOfType<StageHelper>().stageIndex += 1;
+                this.enabled = false;
+            }
+        }
+        if (Input.mouseScrollDelta.y < 0)
+        {
+            targetPosition.y -= Time.deltaTime * moveSpeed;
+            targetPosition.y = Mathf.Clamp(targetPosition.y, endPosition.y, startPosition.y);
+            transform.position = targetPosition;
+            if (transform.position == endPosition)
+            {
+                FindObjectOfType<StageHelper>().stageIndex += 1;
+                this.enabled = false;
+            }
+        }
     }
 }
---
+++ Assets/Scripts/TimeScaleSlider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeScaleSlider : MonoBehaviour
+{
+    private Slider scale;
+    private void Start()
+    {
+        scale = GetComponent<Slider>();
+    }
+    void Update()
+    {
+        Time.timeScale = 1 + scale.value * 10;
+    }
+}
